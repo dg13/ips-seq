4f9d13e895e8bf0b32d53dad3d5fdbde85326b45
Daniel Gaffney <dg13@sanger.ac.uk>
2019-06-22 10:58:03 +0100

    {DJG} Removed test master.sh script

--- master.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-## 1. Copy over raw call files
-SRCDIR=/lustre/scratch116/vr/user/pd3/hipsci/exome-point-mutations/unified-set/final-set-2018-02-05.all
-rsync -av --progress $SRCDIR/exomes.ft.txt.gz Data/newCalls2/raw/
-rsync -av --progress $SRCDIR/high-vs-low-exomes.ft.txt.gz Data/newCalls2/raw/
-rsync -av --progress $SRCDIR/wgs.396.ft.txt.gz Data/newCalls2/raw/
